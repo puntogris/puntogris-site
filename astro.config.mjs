adc9caacc6a19ae68afd87715618c7a2d9805b98
puntogris <rosariodota3@gmail.com>
2023-01-07 12:54:10 -0300

    testing ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
 });
