384eb50a62160a2f4399db445a6bc510bd2c8bd9
puntogris <rosariodota3@gmail.com>
2023-12-08 19:29:11 -0300

    removed unused components and fixed home timer not working

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -1,3 +1,26 @@
+<script>
+  let date = getArgentinaDateTime();
+
+  function getArgentinaDateTime() {
+    try {
+      const date = new Date().toLocaleTimeString("en-US", {
+        timeZone: "America/Argentina/Buenos_Aires",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+        weekday: "short",
+      });
+      return `${date} (UTC-3)`;
+    } catch (error) {
+      return "Error getting date.";
+    }
+  }
+
+  setInterval(() => {
+    date = getArgentinaDateTime();
+  }, 1000);
+</script>
+
 <ul class="text-sm text-gray-100">
   <li class="flex items-center">
     <svg
@@ -39,7 +62,8 @@
       id="date-time-element"
       href="https://time.is/UTC-3"
       target="_blank"
-      class="ml-2 normal-case hover:text-blue-400 hover:underline"></a>
+      class="ml-2 normal-case hover:text-blue-400 hover:underline">{date}</a
+    >
   </li>
   <li class="mt-1 flex items-center">
     <svg
@@ -88,31 +112,3 @@
     >
   </li>
 </ul>
-
-<script>
-  function getArgentinaDateTime() {
-    try {
-      const date = new Date().toLocaleTimeString("en-US", {
-        timeZone: "America/Argentina/Buenos_Aires",
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-        weekday: "short",
-      });
-      return `${date} (UTC-3)`;
-    } catch (error) {
-      return "Error getting date.";
-    }
-  }
-
-  function setDateInElement(element: Element | null) {
-    if (element) {
-      element.textContent = getArgentinaDateTime();
-    }
-  }
-
-  const element = document.getElementById("date-time-element");
-
-  setDateInElement(element);
-  setInterval(() => setDateInElement(element), 1000);
-</script>
