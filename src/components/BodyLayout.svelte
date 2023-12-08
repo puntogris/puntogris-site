bf29ffee0338ff390d9ee129cbe49e5ae6ca40ae
puntogris <rosariodota3@gmail.com>
2023-12-08 17:24:57 -0300

    added sidebar

---
+++ src/components/BodyLayout.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Sidebar from "./Sidebar.svelte";
+
+  export let pathname;
+
+  let width;
+  $: showIfPosible = false;
+  $: isSmallerThanLG = width <= 1024;
+</script>
+
+<svelte:window bind:innerWidth={width} />
+
+<body class="flex min-h-screen w-full flex-row">
+  <Sidebar {pathname} {isSmallerThanLG} bind:showIfPosible />
+  <div class="ml-0 w-full lg:ml-64">
+    <div class="flex h-10 items-center px-3">
+      {#if isSmallerThanLG}
+        <button
+          on:click={() => (showIfPosible = !showIfPosible)}
+          class="block h-6 w-6 rounded-md bg-white">H</button
+        >
+      {/if}
+    </div>
+    <div class="container mx-auto min-h-screen max-w-3xl px-0 lg:px-4">
+      <slot />
+    </div>
+  </div>
+</body>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,92 @@
+<script>
+  export let showIfPosible;
+  export let pathname;
+  export let isSmallerThanLG;
+
+  let sidebarStyle;
+
+  $: {
+    if (showIfPosible && isSmallerThanLG) {
+      sidebarStyle =
+        "bg-gray-900 fixed flex min-h-screen w-64 shrink-0 flex-col border-r border-gray-700 before:absolute before:left-64 before:top-1/2 before:h-full before:w-screen before:-translate-y-1/2 before:bg-gray-400 before:bg-opacity-30";
+    } else if (isSmallerThanLG) {
+      sidebarStyle = null;
+    } else {
+      sidebarStyle =
+        "fixed flex min-h-screen w-64 shrink-0 flex-col border-r border-gray-700 max-lg:hidden";
+    }
+  }
+</script>
+
+{#if sidebarStyle}
+  <div class={sidebarStyle}>
+    <a
+      class="border-b border-gray-700 p-2 text-xl font-bold text-white"
+      href="/"
+      >Puntogris
+
+      {#if isSmallerThanLG}
+        <button
+          on:click={() => (showIfPosible = !showIfPosible)}
+          class="text-white"
+        >
+          close
+        </button>
+      {/if}
+    </a>
+
+    <nav class="flex flex-col px-4 py-3">
+      {#if pathname === "/"}
+        <a
+          class="relative rounded-md bg-gray-700 bg-opacity-30 px-2 py-1.5 text-sm text-white before:absolute before:-left-2 before:top-1/2 before:h-6 before:w-1 before:-translate-y-1/2 before:rounded before:bg-blue-500 hover:bg-gray-800"
+          href="/"
+        >
+          Home
+        </a>
+      {:else}
+        <a
+          class="relative rounded px-2 py-1.5 text-sm text-white hover:bg-gray-800"
+          href="/"
+        >
+          Home
+        </a>
+      {/if}
+      {#if pathname === "/projects"}
+        <a
+          class="relative rounded-md bg-gray-700 bg-opacity-30 px-2 py-1.5 text-sm text-white before:absolute before:-left-2 before:top-1/2 before:h-6 before:w-1 before:-translate-y-1/2 before:rounded before:bg-blue-500 hover:bg-gray-800"
+          href="/projects"
+        >
+          Projects
+        </a>
+      {:else}
+        <a
+          class="relative rounded px-2 py-1.5 text-sm text-white hover:bg-gray-800"
+          href="/projects"
+        >
+          Projects
+        </a>
+      {/if}
+      <h2 class="mt-1 p-2 text-xs font-bold text-gray-400">Social</h2>
+      <a
+        class="flex items-center gap-x-2.5 rounded px-2 py-1.5 text-sm text-white hover:bg-gray-800"
+        href="">Github <span class="text-xs text-gray-500">@puntogris</span></a
+      >
+      <a
+        class="flex items-center gap-x-2.5 rounded px-2 py-1.5 text-sm text-white hover:bg-gray-800"
+        href="">Twitter <span class="text-xs text-gray-500">@puntogris</span></a
+      >
+      <a
+        class="flex items-center gap-x-2.5 rounded px-2 py-1.5 text-sm text-white hover:bg-gray-800"
+        href="">Google <span class="text-xs text-gray-500">@puntogris</span></a
+      >
+    </nav>
+    <div
+      class="mt-auto border-t border-gray-700 px-4 py-2 text-center text-sm text-gray-500"
+    >
+      Copyright © 2024 Puntogris <a
+        href="https://baoshuo.ren"
+        class="hover:text-blue-400">Inspired by Baoshuo</a
+      >
+    </div>
+  </div>
+{/if}
