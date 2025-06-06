c8fd465402af2b5ffbf7477439222b629b513cb3
puntogris <rosariodota3@gmail.com>
2025-06-05 22:29:10 -0300

    migrated to tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+	integrations: [svelte()],
+	vite: {
+		plugins: [tailwindcss()]
+	}
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Inter', ...defaultTheme.fontFamily.sans]
-			},
-			colors: {
-				'dark-blue': '#0d1117'
-			}
-		}
-	},
 	plugins: []
 };
